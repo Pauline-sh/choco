c801e74eb5cd5b27b326940b7ed37c17b5478066
Bryem <pauline-sh-hub@yandex.ru>
2018-09-23 23:44:08 +0300

    changed static root

--- superchoco/settings.py
+++ superchoco/settings.py
@@ -143,11 +143,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'choco/static/'
 
-# Additional locations of static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-STATIC_ROOT = '/home/hosting_russianchoco/projects/russian-souvenir-staging/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '..', "static")
