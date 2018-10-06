9153d74ec632d808d85301bce968f1deb7f3f78b
pauline <pauline-sh-hub@yandex.ru>
2018-10-06 13:32:34 +0300

    added TODO

--- choco/static/choco/js/cart.js
+++ choco/static/choco/js/cart.js
@@ -149,6 +149,9 @@ function addCartItem(e){
         success: function(json) {
             console.log(JSON.stringify(json));
             $('#total-items').text(json.total_items);
+            
+            // TODO:: sidecart reload, two cases 1. cart was empty 2. cart had items
+            reloadSideCart(json.cart);
         },
         error: function(xhr, errmsg, err) {
             console.log(xhr.status + ": " + xhr.responseText);
@@ -156,4 +159,8 @@ function addCartItem(e){
     });
 
     console.log("form submitted!");  // sanity check
+}
+
+function reloadSideCart(cart){
+
 }
