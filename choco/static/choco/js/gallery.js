8561e9037b3babb821b931370b478ebd67455962
Leonidas51 <kirpichik61@gmail.com>
2018-10-10 23:53:51 +0300

    modals for gallery

--- choco/static/choco/js/gallery.js
+++ choco/static/choco/js/gallery.js
@@ -5,7 +5,11 @@ window.addEventListener("load", () => {
         document.querySelector('select[name="configuration"]').firstElementChild.innerHTML = "Выберите характеристики";
     }
 
-    if (!document.getElementById("gallery-selection")) return;
+    document.getElementById("gallery-main").addEventListener("click",openImgModal);
+    document.getElementById("img-modal").addEventListener("click", closeModal);
+    $(".close-modal").each(function() {
+        this.addEventListener("click", closeModal);
+    })
 
     let pictures = document.getElementsByClassName("gallery-small");
     for (let pic of pictures) {
@@ -16,4 +20,23 @@ window.addEventListener("load", () => {
 function replaceMainPic(e) {
     let imglink = e.target.src;
     document.getElementById("gallery-main").firstElementChild.src = imglink;
+}
+
+function openImgModal(e) {
+    let imglink = e.target.src;
+    document.querySelector("#img-modal img").src = imglink;
+    document.querySelector("#img-modal").style.display = "block";
+}
+
+function closeModal(e) {
+    /* clicking outside the modal */
+    if (e.target.parentNode == document.body) {
+        e.target.style.display = "none";
+        return;
+    }
+
+    /* clicking the close button */
+    if (e.target.classList.contains("close-modal")) {
+        e.target.parentNode.parentNode.style.display = "none";
+    }
 }
